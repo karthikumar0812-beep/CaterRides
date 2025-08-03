5fc3d393c2e190cbeadeb1db834946c5b8678e17
karthikumar0812-beep <karthikumar0812@gmail.com>
2025-08-03 17:38:25 +0530

    changed cors

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const organizerRoutes = require("./routes/organizerRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // React app domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/rider", riderRoutes);
