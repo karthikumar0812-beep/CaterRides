c347a2968f3a1a39d60de1e1cae7870b0cac9ec1
karthikumar0812-beep <karthikumar0812@gmail.com>
2025-08-08 23:18:05 +0530

    vaccancy issue solved

--- controllers/respondtoApplication.js
+++ controllers/respondtoApplication.js
@@ -1,6 +1,6 @@
 const Event = require("../models/Events");
 const User = require("../models/User");
-const { sendOrganizerResponseMail } = require("../utils/sendMail")
+const { sendOrganizerResponseMail } = require("../utils/sendMail");
 
 const respondToApplication = async (req, res) => {
   const { eventId, riderId } = req.params;
@@ -21,16 +21,24 @@ const respondToApplication = async (req, res) => {
     }
 
     // Update applicant status in Event
-    const applicant = event.applicants.find(app => app.rider.toString() === riderId);
+    const applicant = event.applicants.find(
+      (app) => app.rider.toString() === riderId
+    );
     if (!applicant) {
-      return res.status(404).json({ message: "Rider did not apply for this event" });
+      return res
+        .status(404)
+        .json({ message: "Rider did not apply for this event" });
     }
     applicant.status = action;
 
     // Update applied event status in User
-    const appliedEvent = user.appliedEvents.find(e => e.eventId.toString() === eventId);
+    const appliedEvent = user.appliedEvents.find(
+      (e) => e.eventId.toString() === eventId
+    );
     if (!appliedEvent) {
-      return res.status(404).json({ message: "Event not found in user's applied list" });
+      return res
+        .status(404)
+        .json({ message: "Event not found in user's applied list" });
     }
     appliedEvent.status = action;
 
@@ -40,34 +48,44 @@ const respondToApplication = async (req, res) => {
       const earnings = Number(user.earnings);
 
       if (isNaN(price) || isNaN(earnings)) {
-        return res.status(400).json({ message: "Invalid negotiatePrice or earnings" });
+        return res
+          .status(400)
+          .json({ message: "Invalid negotiatePrice or earnings" });
       }
 
       user.earnings = earnings + price;
       user.servesCompleted += 1;
+      // Decrement event vacancy
+      if (typeof event.vacancy === "number") {
+        if (event.vacancy > 0) {
+          event.vacancy -= 1;
+        } else {
+          return res
+            .status(400)
+            .json({ message: "No vacancies left for this event" });
+        }
+      }
     }
 
     await event.save();
     await user.save();
 
-    //Email send function 
+    //Email send function
     await sendOrganizerResponseMail(
-  user.email,
-  user.name,
-  event.title,
-  action,
-  event.date,   
-  event.time,  
-  event.location
-);
-
+      user.email,
+      user.name,
+      event.title,
+      action,
+      event.date,
+      event.time,
+      event.location
+    );
 
     res.status(200).json({ message: `Application ${action} successfully` });
-
   } catch (error) {
     console.error("Error responding to application:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-module.exports = {respondToApplication};
+module.exports = { respondToApplication };
