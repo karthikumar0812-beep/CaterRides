1de9da0f6b049685b70ee5b421c4fdb8bd1fb9c6
karthikumar0812-beep <karthikumar0812@gmail.com>
2025-08-15 17:39:46 +0530

    Events sorted successfully

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,16 +1,13 @@
-const Event= require("../models/Events");
-
-
 const getAllEvents = async (req, res) => {
   try {
-    console.log("🔍 Fetching all events...");
-    const events = await Event.find().populate("createdBy", "name email");
+    console.log("🔍 Fetching all events in ascending order...");
+    const events = await Event.find()
+      .populate("createdBy", "name email")
+      .sort({ eventDate: 1 }); // 1 for ascending, -1 for descending
+
     res.status(200).json(events);
   } catch (error) {
     console.error("🔥 Error fetching events:", error);
     res.status(500).json({ error: "Failed to fetch events" });
   }
 };
-
-
-module.exports = { getAllEvents };
